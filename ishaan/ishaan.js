1a926a3d1fa5c8536867600dd07d2d824e64b54b
Ishaan Saha <40528898+ishaaneagles@users.noreply.github.com>
2023-09-15 21:34:41 -0700

    made an effect when hovering over the image on my webpage
    check out my webpage–hovering the mouse over it causes a blue light to form along the image. i can work to make it an animation so that the light fades in and out instead of just appearing and disappearing. we can also use this on the main page after we get all the photos of us. we can hover over each photo and produce the same effect, so that users will know to click the photos in order to access each of our individual webpages.

--- ishaan/ishaan.js
+++ ishaan/ishaan.js
@@ -1,3 +1,12 @@
 function toIndex() {
     window.location.href = '../Index/index.html';
 }
+function imgHover() {
+    const img = document.getElementById("irithyll");
+    img.style.border = "thick solid #00008B";
+}
+
+function imgNoHover() {
+    const img = document.getElementById("irithyll");
+    img.style.border = "";
+}
