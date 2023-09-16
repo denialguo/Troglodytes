216acca34175fc9eb5687b9eb3531a58702e03e0
Ishaan Saha <40528898+ishaaneagles@users.noreply.github.com>
2023-09-15 22:06:48 -0700

    nvm i animated it (its kinda bugged tho)
    yeah so it seems to work every alternate time (the 1st, 3rd, 5th, etc.) i  move my mouse over and image and then away from it??? idk but other than that it's working quite well :)

--- ishaan/ishaan.js
+++ ishaan/ishaan.js
@@ -1,12 +1,14 @@
 function toIndex() {
     window.location.href = '../Index/index.html';
 }
+//BUG: it works alright after hovering over the image and then off of it, but you do it again and it doesn't work. it seems to work every alternate time i move my mouse into and out of the image.
 function imgHover() {
     const img = document.getElementById("irithyll");
-    img.style.border = "thick solid #00008B";
+    img.classList.toggle("fadeInImgAnimation");
+    console.log("on the image");
 }
-
 function imgNoHover() {
     const img = document.getElementById("irithyll");
-    img.style.border = "";
+    img.classList.toggle("fadeOutImgAnimation");
+    console.log("off the image");
 }
