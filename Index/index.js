e2ebee8e32af22584181d96b6e799291fd3a1700
Ishaan Saha <40528898+ishaaneagles@users.noreply.github.com>
2023-09-20 12:00:47 -0700

    added buttons that link to everyone's webpage (except akash)
    put the money in a bag
    this the sound of a bag

--- Index/index.js
+++ Index/index.js
@@ -1,8 +1,17 @@
 function toIshaan() {
-    window.location.href = '../ishaan/ishaan.html'
+    window.location.href = '../ishaan/ishaan.html';
+}
+function toLucas() {
+    window.location.href = '../lucas/lucas.html';
+}
+function toRajdeep() {
+    window.location.href = '../rajdeep/index.html';
+}
+function toAkash() {
+    alert('akash quit holdin out on us and give us your sexy webpage!!!11');
 }
 function alertButton() {
-    window.location.replace("../daniel/Daniel.html")
+    window.location.replace("../daniel/Daniel.html");
 }
 
 function sanWeb() {
